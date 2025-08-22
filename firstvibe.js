11764415a75f5dd4c42a8c5625f2658276f34ef1
kst <kst@ksts-MacBook-Pro.local>
2025-08-22 17:56:40 +0900

    🐛 API 키 입력 타입을 invisible로 변경하여 개행 중복 출력 문제 해결

--- firstvibe.js
+++ firstvibe.js
@@ -599,7 +599,7 @@ class PRDGenerator {
     console.log(chalk.blue('   👉 https://platform.openai.com/account/api-keys\n'));
 
     const response = await prompts({
-      type: 'invisible',
+      type: 'password',
       name: 'apiKey',
       message: '🔑 OpenAI API 키를 입력해주세요 (sk-로 시작):',
       validate: value => {
